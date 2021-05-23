02eaac733a5da85aee430a0fcfee0099a62f6692
Rotesh Chhabra <rt553741@dal.ca>
2021-05-23 16:38:07 +0530

    individual branch created for tutorial2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <h1>
-                    It Works!
-                </h1>
-            </header>
-            <p>
-                Group 8 - Members:
-            </p>
-            <table>
-                <tbody>
-                <tr>
-                    <td>Kartik Gevariya (B00875617)</td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>It Works!</h1>
+      </header>
+      <p>Group 8 - Members:</p>
+      <table>
+        <tbody>
+          <tr>
+            <td>Kartik Gevariya (B00875617)</td>
+            <td>Rotesh Chhabra (B00881776)</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default App;
